7c4092d4fc9c8ec6f437b11417e58a4cdb74537b
CoBoSol <ycocab@gmail.com>
2025-08-15 16:36:33 -0400

    CoboChat

--- portal_cobosis/asgi.py
+++ portal_cobosis/asgi.py
@@ -6,11 +6,19 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chatbot.routing  # Importa las rutas de tu app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal_cobosis.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chatbot.routing.websocket_urlpatterns
+        )
+    ),
+})
