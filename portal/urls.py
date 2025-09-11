75795d7e6b04ee6f3035cce41ac9c4492e41ea19
CoBoSol <ycocab@gmail.com>
2025-09-11 08:43:34 -0400

    Con chatbot http para CPanel

--- portal/urls.py
+++ portal/urls.py
@@ -1,6 +1,8 @@
 #from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 #from equipo.views import ListMiembros, ListTiendas
 
 
@@ -21,3 +23,5 @@ urlpatterns = [
     path('test-item/', views.test_item, name='test_item'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
